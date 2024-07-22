545cf1952cc8e3caad26c6b3f0406da8fae3d8d5
TI Sabbir <tisabbir0@gmail.com>
2024-07-22 06:35:24 +0600

    feat(Hard Parts of JavaScript\ Principles of JS) : Hard Parts of JS is added

---
+++ Hard-Parts-Of-JavaScript/01. Principles of JavaScript.js
@@ -0,0 +1,10 @@
+const num = 3;
+function multiplyBy2 (inputNumber){
+const result = inputNumber*2;
+return result;
+}
+const output = multiplyBy2(num);
+const newOutput = multiplyBy2(10);
+
+console.log(output);
+console.log(newOutput);
