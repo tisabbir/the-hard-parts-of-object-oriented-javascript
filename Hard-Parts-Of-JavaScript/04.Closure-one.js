298cdcf6e0fe13d8044ad3f49b560e8ca6836029
TI Sabbir <tisabbir0@gmail.com>
2024-07-23 06:12:20 +0600

    add (04.closure-one): individual backpack or closure is understood

--- Hard-Parts-Of-JavaScript/04.Closure-one.js
+++ Hard-Parts-Of-JavaScript/04.Closure-one.js
@@ -10,7 +10,14 @@ const myNewFunction = outer();
 
 // console.log(myNewFunction);
 
-console.log(myNewFunction());
-console.log(myNewFunction());
-console.log(myNewFunction());
-console.log(myNewFunction());
+console.log(myNewFunction()); //1
+console.log(myNewFunction()); //2
+console.log(myNewFunction());//3
+console.log(myNewFunction()); //4
+
+const anotherFunction = outer();
+console.log(anotherFunction()); //1
+console.log(anotherFunction()); //2
+
+console.log(myNewFunction()); //5
+console.log(anotherFunction()); //3
