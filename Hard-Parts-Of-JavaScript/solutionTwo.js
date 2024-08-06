e2c21dbe079f743c0eb92091e53563076969f980
TI Sabbir <tisabbir0@gmail.com>
2024-08-06 18:58:49 +0600

    a protoType chain is linked via object.create(userFunctionStore) object creator

--- Hard-Parts-Of-JavaScript/solutionTwo.js
+++ Hard-Parts-Of-JavaScript/solutionTwo.js
@@ -15,4 +15,9 @@ const userFunctionStore = {
 };
 const user1 = userCreator("Will", 3);
 const user2 = userCreator("Tim", 5);
+console.log(user1.score);
 user1.increment();
+console.log(user1.score);
+
+
+
