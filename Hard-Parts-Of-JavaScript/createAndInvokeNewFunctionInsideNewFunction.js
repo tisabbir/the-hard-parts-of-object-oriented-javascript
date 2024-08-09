cf6945dcb26798b7635ffa12986e125a120cfc03
TI Sabbir <tisabbir0@gmail.com>
2024-08-09 23:01:47 +0600

    create and invoke new function inside increament function

---
+++ Hard-Parts-Of-JavaScript/createAndInvokeNewFunctionInsideNewFunction.js
@@ -0,0 +1,15 @@
+function userCreator(name, score) {
+    const newUser = Object.create(userFunctionStore);
+    newUser.name = name;
+    newUser.score = score;
+    return newUser;
+  };
+  const userFunctionStore = {
+    increment: function() {
+        function add1(){ this.score++; }
+        add1()
+    }
+  };
+  const user1 = userCreator("Will", 3);
+  const user2 = userCreator("Tim", 5);
+  user1.increment();
