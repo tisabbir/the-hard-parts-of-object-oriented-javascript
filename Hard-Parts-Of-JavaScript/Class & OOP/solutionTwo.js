9500e0a6888368255e17209ff8452b2c60ebd8c4
TI Sabbir <tisabbir0@gmail.com>
2024-08-11 07:58:14 +0600

    add : solution two recall

--- Hard-Parts-Of-JavaScript/Class & OOP/solutionTwo.js
+++ Hard-Parts-Of-JavaScript/Class & OOP/solutionTwo.js
@@ -1,7 +1,18 @@
 function userCreator (name, score) {
     const newUser = Object.create(userFunctionStore);
+
+    newUser.name = name;
+    newUser.score = score;
+    return newUser;
 }
 
 const userFunctionStore = {
     increment : function (){this.score++}
 }
+
+const user1 = userCreator('Tim', 5);
+
+console.log(user1.name);
+console.log(user1.score);
+user1.increment();
+console.log(user1.score);
