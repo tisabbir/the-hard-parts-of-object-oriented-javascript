cde7283ebd241a96afe2d4f2c24f5cb565fcc801
TI Sabbir <tisabbir0@gmail.com>
2024-08-11 08:17:47 +0600

    parts of class keyword created object+function combination

--- Hard-Parts-Of-JavaScript/Class & OOP/solutionFour.js
+++ Hard-Parts-Of-JavaScript/Class & OOP/solutionFour.js
@@ -1,17 +1,25 @@
-class UserCreator {
-    constructor (name, score) {
+class UserCreator { // function + object combination
+    constructor (name, score) { // the function part
         this.name = name,
         this.score = score
     }
 
-  increment () {
+    // the object part
+    
+    // the object.prototype part
+  increment () { // the increment function is stored in the UserCreator.prototype object
         this.score++
     }
 }
 
-const user1 = new UserCreator('Tim', 5);
+const user1 = new UserCreator('Tim', 5); // new class automates the hard works 
+//1. create a this object
+//2. link the this object with userCreator.object
+//3. return the this object
 
 console.log(user1.name);
 console.log(user1.score);
 user1.increment();
 console.log(user1.score);
+
+console.log(UserCreator.prototype.increment); 
